54fa920f2059e6c1aeb53f8d85c893cd8074cf7f
Jhoysmer <jhoysmer2007@gmail.com>
2025-06-11 23:31:26 -0300

    Tikects

---
+++ tp2.js
@@ -0,0 +1,23 @@
+/*Determinar cual de los dos elementos de texto es mayor
+Utilizando exclusivamente los dos valores booleanos del array, determinar los operadores
+necesarios para obtener un resultado true y otro resultado false
+Determinar el resultado de las cinco operaciones matemáticas realizadas con los dos
+elementos numéricos*/
+var valores = [true, 5, false, "hola", "adios", 2]
+if (5 > 2){
+    console.log(true);
+} else if (2 > 5) {
+    console.log(false);
+} else if (2 == 2){
+    console.log(es_igual);
+} else if (5 == 5){
+    console.log(true);
+}  else if (5 == 2){
+    console.log(false);
+}  else if (2 == 5){
+    console.log(false);
+}  else if ("hola" < "adios"){
+    console.log(true);
+} else if ("hola" > "adios"){
+    console.log(false);
+}
